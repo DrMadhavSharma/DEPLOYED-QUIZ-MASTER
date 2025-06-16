c28e5fd297e7e794936bc4d5d9c1cb97c8d13a83
MADHAV SHARMA <madhavsharma8194@gmail.com>
2025-06-16 22:33:21 +0530

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -1,4 +1,5 @@
-broker_url = "redis://localhost:6379/0"
-result_backend = "redis://localhost:6379/1"
-Timezone = "Asia/kolkata"
+import os
+
+broker_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+result_backend = broker_url
 broker_connection_retry_on_startup = True
